a7019ab5a0d807729b4162b5c007fa94f11e71fb
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-05-19 15:44:33 -0300

    Resolução desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,24 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayDeNumeros) {
+  let contador = 0;
+  let numeroTelefone = arrayDeNumeros.join('').replace(/(\d{2})?(\d{5})/, '($1) $2-');
+  if (arrayDeNumeros.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let numero of arrayDeNumeros) {
+    if (numero < 0 || numero > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (let number of arrayDeNumeros) {
+      if (numero === number) {
+        contador += 1;
+      }
+    }
+    if (contador > 2) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    } contador = 0;
+  }
+  return numeroTelefone;
 }
 
 // Desafio 12
